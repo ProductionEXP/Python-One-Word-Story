daef6646a23f8d725a960242f0536055ba0b9581
Bryce Bartlo <114547919+ProductionEXP@users.noreply.github.com>
2023-04-11 10:08:48 -0700

    Update main.py

--- MacOS/main.py
+++ MacOS/main.py
@@ -3,16 +3,18 @@ import time
 os.system('clear')
 while True:
     os.system('clear')
-    with open('Python-One-Word-Story\\MacOS\\Story.txt') as file:
+    with open('Story.txt') as file:
         contents = file.read()
         search_word = 'EndStory'
-        if search_word in contents
+        if search_word in contents:
             quit()
 
     print('What is your name?')
     N = input()    
 
-    with open('Python-One-Word-Story\\MacOS\\Names.txt') as file3:
+    os.system('clear')
+
+    with open('Names.txt') as file3:
         contents = file3.read()
         search_word = N
         if search_word in contents:
@@ -24,7 +26,7 @@ while True:
 
     if N == 'SeeStory':
         os.system('clear')
-        file1 = open('Python-One-Word-Story\\MacOS\\Story.txt','r'
+        file1 = open('Story.txt','r')
         file1.seek(0)
 
         print('Current Story is ')
@@ -37,7 +39,7 @@ while True:
         continue
 
     if search_word not in contents:
-        file1 = open('Python-One-Word-Story\\MacOS\\Story.txt','r')
+        file1 = open('Story.txt','r')
         file1.seek(0)
 
         print('Current Story is ')
@@ -46,13 +48,14 @@ while True:
 
         file1.close()
 
-        print('What is the one word that you would like to add to the story?')
-        print('Choose carefully, you can only add one.')
+        print('What is the next word in the story?')
         L = input()
-        file1 = open('Python-One-Word-Story\\MacOS\\Story.txt','a')
+        file1 = open('Story.txt','a')
+        file1.writelines(' ')
+        file1.writelines(L)
 
     if len(L.split()) == 1:
-        file2 = open('Python-One-Word-Story\\MacOS\\Names.txt','a')
+        file2 = open('Names.txt','a')
         file2.writelines(N)
         file2.writelines('\n')
         file2.close()
@@ -64,7 +67,3 @@ while True:
         continue
 
     os.system('clear')
-
-    # Started at 5:45pm 10/4/2023
-    # Finished at 8:57pm 10/4/2023
-    # I know this code is probably shit
