b5f030363498f577a59884ca1be2ebbe5aca62de
Bryce Bartlo <114547919+ProductionEXP@users.noreply.github.com>
2023-04-11 08:05:45 -0700

    Minnor Changes

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ import time
 os.system('cls')
 while True:
     os.system('cls')
-    with open("D:\Documents\Github\Python-One-Word-Story\Story.txt") as file:
+    with open('D:\Documents\Github\Python-One-Word-Story\Story.txt') as file:
         contents = file.read()
-        search_word = "EndStory"
+        search_word = 'EndStory'
         if search_word in contents:
             quit()
 
-    print("What is your name?")
+    print('What is your name?')
     N = input()    
 
-    with open("D:\\Documents\\Github\\Python-One-Word-Story\\Names.txt") as file3:
+    with open('D:\\Documents\\Github\\Python-One-Word-Story\\Names.txt') as file3:
         contents = file3.read()
         search_word = N
         if search_word in contents:
-            print("You have already added your word")
-            print("If you wish to see the story put your name in as SeeStory")
+            print('You have already added your word')
+            print('If you wish to see the story put your name in as SeeStory')
             time.sleep(2.5)
             os.system('cls')
             continue
 
-    if N == "SeeStory":
+    if N == 'SeeStory':
         os.system('cls')
-        file1 = open("D:\Documents\Github\Python-One-Word-Story\Story.txt","r")
+        file1 = open('D:\Documents\Github\Python-One-Word-Story\Story.txt','r')
         file1.seek(0)
 
-        print( "Current Story is ")
+        print('Current Story is ')
         print(file1.read())
         print()
 
@@ -37,28 +37,28 @@ while True:
         continue
 
     if search_word not in contents:
-        file1 = open("D:\Documents\Github\Python-One-Word-Story\Story.txt","r")
+        file1 = open('D:\Documents\Github\Python-One-Word-Story\Story.txt','r')
         file1.seek(0)
 
-        print( "Current Story is ")
+        print('Current Story is ')
         print(file1.read())
         print()
 
         file1.close()
 
-        print( "What is the one word that you would like to add to the story?")
-        print( "Choose carefully, you can only add one.")
+        print('What is the one word that you would like to add to the story?')
+        print('Choose carefully, you can only add one.')
         L = input()
-        file1 = open("D:\Documents\Github\Python-One-Word-Story\Story.txt","a")
+        file1 = open('D:\Documents\Github\Python-One-Word-Story\Story.txt','a')
 
     if len(L.split()) == 1:
-        file2 = open("D:\\Documents\\Github\\Python-One-Word-Story\\Names.txt",'a')
+        file2 = open('D:\\Documents\\Github\\Python-One-Word-Story\\Names.txt','a')
         file2.writelines(N)
-        file2.writelines("\n")
+        file2.writelines('\n')
         file2.close()
 
     if len(L.split()) != 1:
-        print("You inputed more than one word, try again.")
+        print('You inputed more than one word, try again.')
         time.sleep(2)
         os.system('cls')
         continue
